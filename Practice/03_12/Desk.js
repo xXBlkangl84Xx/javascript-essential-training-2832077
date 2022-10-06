9dd5e64393ffee0c587b79b92032213375d53a0d
ptrck <blkangl84@gmail.com>
2022-10-06 10:24:53 -0400

    stuff

---
+++ Practice/03_12/Desk.js
@@ -0,0 +1,19 @@
+class Desk {
+  constructor(name, width, height, length, color, deskAssembled) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+    this.length = length;
+    this.color = color;
+    this.deskAssembled = deskAssembled;
+  }
+
+  assembleDesk() {
+    this.deskAssembled = true;
+    return console.log("Desk Assembled");
+  }
+  disassembleDesk() {
+    this.deskAssembled = false;
+    return console.log("Desk Disassembled");
+  }
+}
