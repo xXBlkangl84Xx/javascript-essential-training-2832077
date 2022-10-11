512509407053be3ee2834bfc5feec093f13afcf3
ptrck <blkangl84@gmail.com>
2022-10-11 09:40:18 -0400

    more exercises

---
+++ 03_14/Book.js
@@ -0,0 +1,9 @@
+class Book {
+  constructor(Title, Author, Genre, PublishDate) {
+    this.Title = Title;
+    this.Author = Author;
+    this.Genre = Genre;
+    this.PublishDate = PublishDate;
+  }
+}
+export default Book;
--- 03_14/script.js
+++ 03_14/script.js
@@ -1,11 +1,12 @@
 /**
  * Challenge: Create a new object type
- * 
+ *
  * - Create a new object type "Book" using a class or an object constructor function.
  * - Add at least 5 book objects.
  */
 
 import Backpack from "./Backpack.js";
+import Book from "./Book.js";
 
 const everydayPack = new Backpack(
   "Everyday Backpack",
@@ -17,7 +18,38 @@ const everydayPack = new Backpack(
   false,
   "December 5, 2018 15:00:00 PST"
 );
+const bookOne = new Book(
+  "The Fellowship of the Ring",
+  "JRR Tolkien",
+  "Fantasy",
+  "January 20, 1910 12:00:00 EST"
+);
+const bookTwo = new Book(
+  "The Two Towers",
+  "JRR Tolkien",
+  "Fantasy",
+  "March 10, 1912 12:00:00 EST"
+);
+const bookThree = new Book(
+  "The Return of the King",
+  "JRR Tolkien",
+  "Fantasy",
+  "April 30, 1914 12:00:00 EST"
+);
+const bookFour = new Book(
+  "12 Rules for Life",
+  "Peterson",
+  "Non-Fiction",
+  "March 29, 2018 12:00:00 EST"
+);
+const bookFive = new Book(
+  "The Unicorn Project",
+  "Kim",
+  "Fiction",
+  "January 20, 2022 12:00:00 EST"
+);
 
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
 console.log("Days since aquired:", everydayPack.backpackAge());
+console.log("Books:", bookOne, bookTwo, bookThree, bookFour, bookFive);
